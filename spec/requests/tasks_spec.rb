58d7ac4e21f6aa0bc013e58a179cb215fdbd15df
Vance <vance.goldenowl@gmail.com>
2023-09-07 17:33:56 +0700

    Add request test

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Tasks", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Tasks', type: :request do
+  describe 'GET /index' do
+    it 'render index template' do
+      get tasks_path
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
